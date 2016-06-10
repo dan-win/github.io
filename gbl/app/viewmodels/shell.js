725fcef1d46565c553987e574d04d1a6e8d06368
Danwin <dmitry.zimogyadov@gmail.com>
2016-06-11 00:33:06 +0300

    css updated

--- gbl/app/viewmodels/shell.js
+++ gbl/app/viewmodels/shell.js
@@ -4,24 +4,24 @@ define(['plugins/router', 'durandal/app'], function (router, app) {
 		activate: function () {
 			router.map([
 				// default route:
-				{ route: '', title:'Homepage', moduleId: 'viewmodels/application-page-1', 
-					hash: '#homepage'}
+				{ route: '', title:'Apply Now', moduleId: 'viewmodels/application-page-1', nav: false,
+					hash: '#application-page-1'}
 				// dynamic route, depends on "pageNo", hash for history :
-				,{ route: 'application-page-1', title:'Apply Now', moduleId: 'viewmodels/application-page-1', nav: false, 
-					hash: '#app-page-1' }
+				,{ route: 'application-page-1', title:'Apply Now', moduleId: 'viewmodels/application-page-1', nav: true, 
+					hash: '#application-page-1' }
 
 				,{ route: 'application-page-2/:appId', title:'Application Page 2', moduleId: 'viewmodels/application-page-2', nav: false }
 				,{ route: 'application-page-3/:appId', title:'Application Page 3', moduleId: 'viewmodels/application-page-3', nav: false }
 
 				// Visible in navigation:
-				,{ route: 'about-us', title:'About', moduleId: 'viewmodels/about-us', nav: true }
+				,{ route: 'about-us', title:'About', moduleId: 'viewmodels/about-us', nav: true, hash: '#about-us' }
 				,{ route: 'contact-us', title:'Contact', moduleId: 'viewmodels/contact-us', nav: true, hash: '#contact-us' }
 				,{ route: 'faq', title:'FAQ', moduleId: 'viewmodels/faq', nav: true, hash: '#faq' }
 
 				// Service pages (invisible from navigation)
 				,{ route: 'error-page(/:appId)', title:'Error', moduleId: 'viewmodels/error-page', nav: false }
-				,{ route: 'privacy-policy', title:'Privacy Policy', moduleId: 'viewmodels/privacy-policy', nav: false }
-				,{ route: 'terms-n-conditions', title:'Terms and Conditions', moduleId: 'viewmodels/terms-n-conditions', nav: false }
+				,{ route: 'privacy-policy', title:'Privacy Policy', moduleId: 'viewmodels/privacy-policy', nav: false, hash:'#privacy-policy' }
+				,{ route: 'terms-n-conditions', title:'Terms and Conditions', moduleId: 'viewmodels/terms-n-conditions', nav: false, hash:'terms-n-conditions' }
 				,{ route: 'thank-you(/:appId)', title:'Thank You', moduleId: 'viewmodels/thank-you', nav: false }
 
 				,{ route: 'non-ho-thank-you(/:appId)', title:'Thank You', moduleId: 'viewmodels/thank-you', nav: false }
